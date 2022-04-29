8b2a70b3a51d584177f83b5698f605de23e99592
Logan Snopek <LSnopek@lcs.on.ca>
2022-04-29 11:11:51 -0400

    Initial Commit

---
+++ SliderAnimation/SliderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAnimationApp.swift
+//  SliderAnimation
+//
+//  Created by Logan Snopek on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct SliderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
