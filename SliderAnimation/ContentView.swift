23c811b4277f4f4822b7b041e0f6ea7866f5ff7c
Logan Snopek <LSnopek@lcs.on.ca>
2022-04-30 19:40:49 -0400

    Done with the animation

--- SliderAnimation/ContentView.swift
+++ SliderAnimation/ContentView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Stored Proprotys
+    
+    @State var hight = 100.0
+    let width = 100.0
+    
+    //MARK: Computed Proprotys
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack{
+            RoundedRectangle(cornerRadius: 200)
+                .padding()
+                .frame(width: width, height: hight)
+            
+                .animation(
+                    Animation
+                        .easeInOut(duration: 2)
+                        .repeatForever(autoreverses: true)
+                )
+            
+            
+            RoundedRectangle(cornerRadius: 200)
+                .padding()
+                .frame(width: width, height: hight)
+            
+                .animation(
+                   Animation
+                    .easeInOut(duration: 4)
+                    .repeatForever(autoreverses: true)
+                )
+            
+            
+            RoundedRectangle(cornerRadius: 200)
+                .padding()
+                .frame(width: width, height: hight)
+            
+                .animation(
+                    Animation
+                        .easeInOut(duration: 5)
+                        .repeatForever(autoreverses: true)
+                )
+
+        }
+        .onTapGesture {
+            hight = 500
+        }
+        
+        
+        
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
